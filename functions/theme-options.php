752ea126395368b03b8dfdc6314fa6db90e0839e
AlxMedia <contact@alx.media>
2022-02-23 12:10:04 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -549,7 +549,7 @@ Kirki::add_field( 'noteblock_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'noteblock' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'noteblock' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'noteblock' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'noteblock' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'noteblock' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'noteblock' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'noteblock' ),
@@ -566,7 +566,7 @@ Kirki::add_field( 'noteblock_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'noteblock' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'noteblock' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'noteblock' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'noteblock' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'noteblock' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
