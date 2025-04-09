21c3bc1cc58ed182aef84b15e5759f166149cf63
Alexander Agnarson <contact@alx.media>
2025-04-09 20:24:05 +0200

    Kirki deprecated fix

--- functions.php
+++ functions.php
@@ -731,3 +731,16 @@ function noteblock_skip_link_focus_fix() {
 	<?php
 }
 add_action( 'wp_print_footer_scripts', 'noteblock_skip_link_focus_fix' );
+
+
+/*  Kirki deprecated fix
+/* ------------------------------------ */
+function noteblock_kirki_config( $config ) {
+
+	if ( isset( $config['compiler'] ) ) {
+		unset( $config['compiler'] );
+	}
+
+	return $config;
+}
+add_filter( 'kirki/config', 'noteblock_kirki_config', 999 );
